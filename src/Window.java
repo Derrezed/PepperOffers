9efbc56782d4d4c6c4b187940d6b05e50243e8ac
Grzegorz Tkaczyk <grzegorztkaczyk97@gmail.com>
2019-02-21 01:14:32 +0100

    Fixing problems with JTable

--- src/Window.java
+++ src/Window.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 public class Window extends JFrame implements ActionListener {
 
-    private JButton getAnswer = new JButton("Get results");
+    private boolean flag = false;
+    private JButton getAnswer = new JButton("Refresh");
     private OfferList pepperek = new OfferList("https://www.pepper.pl/");
     private JTable table;
     private String[] columnNames = {"Name", "Temperature"};
@@ -24,25 +25,42 @@ public class Window extends JFrame implements ActionListener {
         getAnswer.addActionListener(this::actionPerformed);
         table = new JTable(tableModel);
         add(table);
+        puttingData();
+        System.out.println(tableModel.getRowCount() + " ilosc wierszy");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-
         if(source == getAnswer) {
+            if(!tableModel.getValueAt(1,1).equals(null)) {
+                int tableSize = tableModel.getRowCount();
+                for(int i = 0; i < tableSize; ++i) {
+                    tableModel.removeRow(0);
+                }
+                puttingData();
+            }
+        }
+    }
+
+    public boolean puttingData() {
+        try {
             pepperek.showList();
-            ArrayList<Offer> offerList =  pepperek.getList();
+            ArrayList<Offer> offerList = pepperek.getList();
             int row = 0;
             String name;
             String temp;
-            for(Offer offer : offerList) {
+            for (Offer offer : offerList) {
                 name = offer.getOfferName();
                 temp = offer.getOfferTemp();
-                Object[] data = {name,temp};
+                Object[] data = {name, temp};
                 tableModel.addRow(data);
                 row++;
             }
+            return true;
+        } catch (Exception e){
+            System.out.println(e.toString());
+            return false;
         }
     }
 }
