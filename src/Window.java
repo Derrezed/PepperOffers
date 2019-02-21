c065abbc0b0a69a22d75532014739b88f7333aac
Grzegorz Tkaczyk <grzegorztkaczyk97@gmail.com>
2019-02-21 22:14:55 +0100

    changing GUI

--- src/Window.java
+++ src/Window.java
@@ -8,23 +8,27 @@ import java.util.ArrayList;
 public class Window extends JFrame implements ActionListener {
 
     private boolean flag = false;
+    private JButton refresh = new JButton("Refresh");
     private JButton getAnswer = new JButton("Refresh");
-    private OfferList pepperek = new OfferList("https://www.pepper.pl/");
+    private OfferList pepperek; //= new OfferList("https://www.pepper.pl/");
     private JTable table;
     private String[] columnNames = {"Name", "Temperature"};
     private DefaultTableModel tableModel = new DefaultTableModel(columnNames,0);
+    private JTextField pageInsert;
 
     public Window() {
         super("PepperOffers");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,700);
+        setSize(300,400);
         setLocation(50,50);
         setVisible(true);
-        setLayout(new GridLayout(2,2));
-        add(getAnswer);
-        getAnswer.addActionListener(this::actionPerformed);
+        setLayout(new BorderLayout());
+        add(refresh, BorderLayout.LINE_START);
+        refresh.addActionListener(this::actionPerformed);
         table = new JTable(tableModel);
-        add(table);
+        pageInsert = new JTextField();
+        add(pageInsert, BorderLayout.PAGE_START);
+        add(table, BorderLayout.PAGE_END);
         puttingData();
         System.out.println(tableModel.getRowCount() + " ilosc wierszy");
     }
@@ -32,7 +36,7 @@ public class Window extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if(source == getAnswer) {
+        if(source == refresh) {
             if(!tableModel.getValueAt(1,1).equals(null)) {
                 int tableSize = tableModel.getRowCount();
                 for(int i = 0; i < tableSize; ++i) {
